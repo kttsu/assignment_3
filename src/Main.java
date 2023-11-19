e4b2ce7ef1816484a30ccd9ed96c82d14775afa4
kttsu <150462533+kttsu@users.noreply.github.com>
2023-11-19 23:21:15 +0900

    Revert "Update Main.java"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,15 +16,17 @@ public class Main {
         for (Map.Entry<String, String> entry : countries.entrySet()) {
             System.out.println(entry.getKey() + ":" + entry.getValue());
         }
-        String capital = getCapital(countries, "インド");
-        if ("指定された国は存在しません".equals(capital)) {
-            System.out.println('\n' + capital);
-        } else {
-            System.out.println("インド:" + capital);
-        }
-    }   
 
- private static String getCapital(Map<String, String> map, String country) {
+        try {
+            String capital = getCapital(countries,"インド");
+            System.out.println("インド:" + capital);
+        } catch (NoSuchElementException e) {
+            System.out.print('\n');
+            System.out.println("※指定された国は存在しません");
+        }
+    }
+
+   private static String getCapital(Map<String, String> map, String country) {
         // キーが存在する場合は対応する首都を返す
         // 存在しない場合はデフォルト値を返す
         return map.getOrDefault(country, "指定された国は存在しません");
